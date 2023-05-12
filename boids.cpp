645ffbcda2a274228c85ad148f898aafca665b5e
Gregorio Innocenti <gregorioinnocenti03@gmail.com>
2023-05-12 10:16:31 +0200

    piccoli cambiamenti

--- boids.cpp
+++ boids.cpp
@@ -3,10 +3,21 @@
 #include <cmath>
 
 Vector2d Vector2d::operator+=(Vector2d const& v) {
-  return {this->x + v.x, this->y + v.y};
+  return {this->x + v.x, this->y + v.y}; 
+  
+   //si potrebbe cambiare con 
+   //x += other.x;
+   //y += other.y;
+   //return *this;
+   //?
+
+
+
 }
 
-Vector2d Vector2d::operator*(double const& l) { return {x * l, y * l}; }
+//sostituisco l con scalar
+
+Vector2d Vector2d::operator*(double const& scalar) { return {x * scalar, y * scalar}; }
 
 double Vector2d::length(Vector2d const& v) {
   return {std::sqrt(v.x * v.x + v.y * v.y)};
