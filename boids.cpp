6274134d7d47b46dfc087035eee5f0f5653fc490
Gregorio Innocenti <gregorioinnocenti03@gmail.com>
2023-05-12 16:47:53 +0200

    minor changes

--- boids.cpp
+++ boids.cpp
@@ -42,13 +42,17 @@ Vector2d Boid::GetPosition(Boid const& b) { return b.position_; }
 
 Vector2d Boid::GetVelocity(Boid const& b) { return b.velocity_; }
 
+
+// draw boid
+void Boid::Draw(sf::RenderWindow& window) const {
+  
+}
+
+
+
 // constructor
 Boid::Boid(Vector2d const& pos, Vector2d const& vel, Vector2d const& acc)
     : position_{pos}, velocity_{vel}{
   // create shape
-  shape_.setPointCount(3);
-  shape_.setPoint(0, sf::Vector2f(0, 0));
-  shape_.setPoint(1, sf::Vector2f(20, 10));
-  shape_.setPoint(2, sf::Vector2f(0, 20));
-  shape_.setFillColor(sf::Color::Green);
+ 
 }
