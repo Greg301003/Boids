50146facff65317fc841ed4e72549fe1e5fb6c77
Gregorio Innocenti <gregorioinnocenti03@gmail.com>
2023-05-13 17:45:08 +0200

    Window

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
-int main(){
-    return 0;
+#include "boids.hpp"
+int main() {
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Boids Simulation");
+
+  while (window.isOpen())  // loop
+  {
+    sf::Event event;
+    while (window.pollEvent(event))        
+    {
+      if (event.type == sf::Event::Closed) // close clicking 
+      {
+        window.close();
+      }
+    }
+    window.clear();
+    window.display();
+  }
+
+  return 0;
 }
