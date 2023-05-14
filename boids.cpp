b5f69ab4d22569b6f4738fab2de9e5da2891e559
Gregorio Innocenti <gregorioinnocenti03@gmail.com>
2023-05-14 21:39:00 +0200

    namepaces

--- boids.cpp
+++ boids.cpp
@@ -2,22 +2,20 @@
 
 #include <cmath>
 
-
-  //missing & in vector2d 
-Vector2d operator+=(Vector2d& const& v) {
+// missing & in vector2d
+Vector2d operator+=(Vector2d& /*removed const*/ v) {
   return {this->x + v.x, this->y + v.y};
 
+  Vector2d operator*(double const& scalar) {
+    // missing this->
+    return {this->x * scalar, this->y * scalar};
+  }
 
+  double length(Vector2d const& v) {
+    return {std::sqrt(v.x * v.x + v.y * v.y)};
+  }
+}
 
-Vector2d operator*(double const& scalar) { 
-  // missing this->
-  return
-  {this->x * scalar, this->y * scalar};}
-
-double length(Vector2d const& v) { return {std::sqrt(v.x * v.x + v.y * v.y)}; }
-}  // namespace Vector2d
-
-namespace Boid_NS {
 double Distance(Boid const& b1, Boid const& b2) {
   double dx = b1.position_.x - b2.position_.x;
   double dy = b1.position_.y - b2.position_.y;
@@ -27,7 +25,7 @@ double Distance(Boid const& b1, Boid const& b2) {
 // giving an alternative definition of Boid::Distance by using Vector2d methods
 double Boid::Distance(Boid const& i, Boid const& j) {
   auto distance = i.position_ - j.position_;
-  //misspel lenght
+  // misspel lenght
   return distance.length();
 }
 
@@ -35,4 +33,3 @@ Vector2d GetPosition(Boid const& b) { return b.position_; }
 Vector2d GetVelocity(Boid const& b) { return b.velocity_; }
 
 void Draw(sf::RenderWindow& window) const { window.draw(shape_); }
-}  // namespace Boid
--- boids.hpp
+++ boids.hpp
@@ -5,35 +5,37 @@
 #include <cmath>
 #include <vector>
 
-namespace Vector2d{
+namespace Vector2d {
 class Vector2d {
  public:
   double x{};
   double y{};
 
-  Vector2d operator+=(Vector2d const&);     //somma tra vettori
-  Vector2d operator*(double const&);      //prodotto per scalare
-  double length(Vector2d const&);     //ritorna la lunghezza del vettore
+  Vector2d operator+=(Vector2d const&);  // somma tra vettori
+  Vector2d operator*(double const&);     // prodotto per scalare
+  double length(Vector2d const&);        // ritorna la lunghezza del vettore
 };
-}
-namespace Boid{
+}  // namespace Vector2d
+namespace Boid {
 class Boid {
   Vector2d position_{};
   Vector2d velocity_{};
   sf::ConvexShape shape_;
 
  public:
-  Boid(Vector2d const& pos, Vector2d const& vel, Vector2d const& acc)     //constructor
+  Boid(Vector2d const& pos, Vector2d const& vel,
+       Vector2d const& acc)  // constructor
       : position_{pos}, velocity_{vel} {
-        //create boid shape (choice: triangle)
-      }
+    // create boid shape (choice: triangle)
+  }
 
-  Vector2d GetPosition(Boid const&);      //ritorna la posizione del boid
-  Vector2d GetVelocity(Boid const&);      //ritorna la velocità del boid
+  Vector2d GetPosition(Boid const&);  // ritorna la posizione del boid
+  Vector2d GetVelocity(Boid const&);  // ritorna la velocità del boid
 
-  double Distance(Boid const& b1, Boid const& b2);      //ritorna la distanza tra due boid
+  double Distance(Boid const& b1,
+                  Boid const& b2);  // ritorna la distanza tra due boid
 
-  void Draw(sf::RenderWindow& window) const;      //draw boid
+  void Draw(sf::RenderWindow& window) const;  // draw boid
 };
-}
+}  // namespace Boid
 #endif
