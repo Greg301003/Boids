c0751b19f766e228544f6fd23e9fe310a662d50b
Dario Campisi <dariocampisi15@icloud.com>
2023-05-23 10:35:07 +0200

    minor changes

--- main.cpp
+++ main.cpp
@@ -1,50 +1,53 @@
 #include "boids.hpp"
+
+//TUTTO DA SISTEMARE
+
 int main() {
-  // create window
+  // // create window
 
-  sf::RenderWindow window(sf::VideoMode(1200, 1000), "Boids Simulation");
+  // sf::RenderWindow window(sf::VideoMode(1200, 1000), "Boids Simulation");
 
-  // create a vector of boids
-  std::vector<Boid::Boid> boids;
+  // // create a vector of boids
+  // std::vector<bd::Boid> boids;
 
-  // generate boids
-  for (int i = 0; i < 30; ++i) {
-    // added static cast
+  // // generate boids
+  // for (int i = 0; i < 30; ++i) {
+  //   // added static cast
 
-    Vector2d::Vector2d pos = {static_cast<double>(rand() % 1200),
-                              static_cast<double>(rand() % 1000)};
-    Vector2d::Vector2d vel = {static_cast<double>( ((rand() % 5)-2.5)/100 ),
-                              static_cast<double>(((rand() % 5)-2.5)/100)};
-    Vector2d::Vector2d acc = {0, 0};
+  //   sf::Vector2<float> pos = {static_cast<double>(rand() % 1200),
+  //                             static_cast<double>(rand() % 1000)};
+  //   sf::Vector2<float> vel = {static_cast<double>( ((rand() % 5)-2.5)/100 ),
+  //                             static_cast<double>(((rand() % 5)-2.5)/100)};
+  //   sf::Vector2<float> acc = {0, 0};
 
-    //adding rotation speed
-    double rotation_speed = static_cast<double>(1.0); 
-    Boid::Boid boid(pos, vel, acc, rotation_speed);
-    boids.push_back(boid);
-  }
+  //   //adding rotation speed
+  //   double rotation_speed = static_cast<double>(1.0); 
+  //   bd::Boid boid(pos, vel, acc, rotation_speed);
+  //   boids.push_back(boid);
+  // }
 
-  while (window.isOpen())  // loop
-  {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed)  // close clicking
-      {
-        window.close();
-      }
-    }
-      for (auto& boid : boids) {
-    boid.UpdatePosition();
-    boid.UpdateRotation();
-  }
+  // while (window.isOpen())  // loop
+  // {
+  //   sf::Event event;
+  //   while (window.pollEvent(event)) {
+  //     if (event.type == sf::Event::Closed)  // close clicking
+  //     {
+  //       window.close();
+  //     }
+  //   }
+  //     for (auto& boid : boids) {
+  //   boid.UpdatePosition();
+  //   boid.UpdateRotation();
+  // }
 
-    window.clear();
-    // draw boids
-    for (auto const& boid : boids) {
-      boid.Draw(window);
-    }
+  //   window.clear();
+  //   // draw boids
+  //   for (auto const& boid : boids) {
+  //     boid.Draw(window);
+  //   }
 
-    window.display();
-  }
+  //   window.display();
+  // }
 
-  return 0;
+  // return 0;
 }
